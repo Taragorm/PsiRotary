fe588e2dbb189ddbf3fda2462cd00d61a42da9da
Taragorm <taragorm@zoho.eu>
2025-08-06 17:43:51 +0100

    Fixed; now header only

--- src/RotaryEnc.h
+++ src/RotaryEnc.h
@@ -26,32 +26,75 @@ public:
 };
 
 
+//====================================================================
+class RotaryEncoderActions
+{
+protected:    
+    RotaryEncoderActions() {}
 
+public:    
+    static const char LEFT = 'L';
+    static const char RIGHT = 'R';
+    static const char DOWN = 'D';
+    static const char UP = 'U';
+};
+//====================================================================
 /*!
     basic encoder implementation
  */
-class RotaryEncoderImpl
+template<typename TMR = MilliTimer>
+class RotaryEncoderImpl : RotaryEncoderActions
 {
-    psiiot::Debounce ph1_;
-    psiiot::Debounce ph2_;
-    psiiot::Debounce sw_;
+    psiiot::Debounce<TMR> ph1_;
+    psiiot::Debounce<TMR> ph2_;
+    psiiot::Debounce<TMR> sw_;
+
     bool last_ph1_;
     bool last_sw_;
     const bool upState_;
 
 public:
-    static const char LEFT = 'L';
-    static const char RIGHT = 'R';
-    static const char DOWN = 'D';
-    static const char UP = 'U';
 
     static const bool IMPLEMENTED = true;
 
-    RotaryEncoderImpl(int8_t ph1, int8_t ph2, int8_t sw, bool upState, int debounceMs=5);
+    RotaryEncoderImpl(int8_t ph1, int8_t ph2, int8_t sw, bool upState, int debounceMs)
+    : 
+        ph1_(ph1, debounceMs), 
+        ph2_(ph2, debounceMs), 
+        sw_(sw, debounceMs), 
+        upState_(upState)
+    {
+        pinMode(ph1, INPUT_PULLUP);
+        pinMode(ph2, INPUT_PULLUP);
+        pinMode(sw, INPUT_PULLUP);
+        last_ph1_ = digitalRead(ph1);
+        last_sw_ = digitalRead(sw);
+    }
     //-----------------------------------------------------------------
-    uint8_t pollEncoder();
+    uint8_t pollEncoder()
+    {
+        int8_t ret = 0;
+        auto v = ph1_.read();
+        auto v2 = ph2_.read();
+        
+        if(!v && last_ph1_)
+        {
+            ret = (v2==upState_) ? LEFT : RIGHT;
+        }        
+        last_ph1_ = v;
+        return ret;
+    }
     //-----------------------------------------------------------------
-    uint8_t pollSwitch();
+    uint8_t pollSwitch()
+    {
+        auto v = sw_.read();
+        
+        if(v==last_sw_)
+            return 0;
+
+        last_sw_ = v;
+        return (v==upState_) ? UP : DOWN;
+    }
 
 };
 
@@ -64,14 +107,15 @@ template <
     pin_size_t PH1,
     pin_size_t PH2,
     pin_size_t SW,
-    bool UPSTATE
+    bool UPSTATE,
+    typename TMR = MilliTimer
 >
-class RotaryEncoder : public RotaryEncoderImpl
+class RotaryEncoder : public RotaryEncoderImpl<TMR>
 {
 public:
 
     RotaryEncoder(int debounceMs=5 )
-    : RotaryEncoderImpl(PH1,PH2,SW,UPSTATE, debounceMs)
+    : RotaryEncoderImpl<TMR>(PH1,PH2,SW,UPSTATE, debounceMs)
     {
     }
     //-----------------------------------------------------------------
